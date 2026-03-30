2878433a0baafe727e66cf7f3d563a0911c71eaa
shikeshjayan <shikeshjayan345@gmail.com>
2026-03-30 23:20:45 +0530

    bugFixes: on CORS

--- server/src/server.js
+++ server/src/server.js
@@ -53,7 +53,7 @@ app.use(cookieParser());
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
-  process.env.FRONT_END_URL,
+  "https://movieverse-ai.vercel.app",
 ];
 
 app.use(
