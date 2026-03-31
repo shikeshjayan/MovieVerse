a04f1a62d4a04c53c2731801f02e1f720ad90aa1
shikeshjayan <shikeshjayan345@gmail.com>
2026-03-31 22:17:42 +0530

    bugFixes: Cookies

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -106,8 +106,8 @@ export const login = catchAsync(async (req, res, next) => {
 
   const cookieOptions = {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    secure: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
     path: "/",
   };
